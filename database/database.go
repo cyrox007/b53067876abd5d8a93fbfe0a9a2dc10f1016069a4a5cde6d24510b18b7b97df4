402668eae2f8bdd2d47a98dc8e8a5d12eca44a77
cyrox007 <cyrox007@gmail.com>
2025-03-28 09:53:40 +0300

    env

--- database/database.go
+++ database/database.go
@@ -7,6 +7,7 @@ import (
 
 	"test/models"
 
+	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -14,15 +15,33 @@ import (
 var DB *gorm.DB
 
 func Connect() error {
-	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s",
-		"localhost", "5432", "username", "newsdb", "yourpassword")
+	// Настройка Viper для загрузки .env файла
+	viper.SetConfigFile(".env") // Указываем файл .env
+	viper.AutomaticEnv()        // Разрешаем использование переменных окружения
 
+	if err := viper.ReadInConfig(); err != nil {
+		log.Println("Файл .env не найден, используем переменные окружения системы")
+	}
+
+	// Получение значений переменных окружения через Viper
+	dbHost := viper.GetString("DB_HOST")
+	dbPort := viper.GetString("DB_PORT")
+	dbUser := viper.GetString("DB_USER")
+	dbPassword := viper.GetString("DB_PASSWORD")
+	dbName := viper.GetString("DB_NAME")
+
+	// Формирование строки подключения
+	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s",
+		dbHost, dbPort, dbUser, dbName, dbPassword)
+
+	// Подключение к базе данных
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return fmt.Errorf("ошибка подключения к БД: %v", err)
 	}
 
+	// Получение пула соединений
 	sqlDB, err := DB.DB()
 	if err != nil {
 		return fmt.Errorf("ошибка получения пула соединений: %v", err)
